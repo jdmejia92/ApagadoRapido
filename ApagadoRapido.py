87f9d0dde95f7cdae4ee852cd310054477e2618f
Juan Daniel Mejia Hoyos <jdmejia92@icloud.com>
2023-06-15 17:33:01 -0400

    Adding tqdm for graphic view of the process

--- ApagadoRapido.py
+++ ApagadoRapido.py
@@ -1,18 +1,46 @@
 import subprocess as sp
 import os
+from tqdm import tqdm
 from time import sleep
+import asyncio
 
-class Creating_apps_list():
+
+class View:
+    def progress_bar(self):
+        bar = tqdm(total=100)
+        return bar
+
+    def client_view(self):
+        print("\nShutting down open apps...")
+
+    def asking_for_shutting_pc(self, apps_closed):
+        if apps_closed == False:
+            print("Apps already closed")
+        question = input("Do you wish to shutdown your computer ? (y / n): ")
+        return question
+
+
+class Creating_apps_list:
     def getting_apps(self):
-        lista = sp.run(['powershell.exe', 'gps | ? {$_.MainWindowTitle} | Format-Table Name'], stdout=sp.PIPE).stdout.splitlines()
+        lista = sp.run(
+            ["powershell.exe", "gps | ? {$_.MainWindowTitle} | Format-Table Name"],
+            stdout=sp.PIPE,
+        ).stdout.splitlines()
         return lista
-    
+
     def listing_apps(self, lista):
-        apps_not_to_close = ["WindowsTerminal", "", "SystemSettings", "TextInputHost", "ApplicationFrameHost", "Code"]
+        apps_not_to_close = [
+            "WindowsTerminal",
+            "",
+            "SystemSettings",
+            "TextInputHost",
+            "ApplicationFrameHost",
+            "Code",
+            "Name",
+            "----",
+        ]
         open_apps = []
         if lista:
-            for i in range(3):
-                lista.pop(0)
             for app in lista:
                 decode_string = app.decode("utf-8")
                 program = decode_string.strip()
@@ -20,30 +48,61 @@ class Creating_apps_list():
                     pass
                 else:
                     open_apps.append(program)
-                sleep(0.1)
+                sleep(0.05)
+            open_apps.append("Whatsapp")
         return open_apps
 
 
-class Closing_apps():
-    @staticmethod
-    def close_apps(app_list: list[str]):
+class ShuttingDown:
+    """
+    Class for the logic of shutting the apps and finally the PC
+    """
+    def close_apps(self, app_list: list[str]):
+        """
+        Received a list of apps and proceed to shut them down
+        param:: app_list
+        return:: Bool
+        """
+        result = False
         if app_list:
             for app in app_list:
-                sp.check_call(['powershell.exe', "Stop-Process -NAME", app], stderr=sp.PIPE)
+                sp.check_call(
+                    ["powershell.exe", "Stop-Process -NAME", app], stderr=sp.PIPE
+                )
                 sleep(0.09)
-            close = input("Do you wish to shutdown your computer ? (y / n): ")
-            if close.lower() == "y":
-                os.system("shutdown /s /t 1")
-            else:
-                exit()
+            result = True
+        return result
+
+    def shut_pc(self, answer):
+        """
+        Shut down the PC if get the right input
+        param:: string
+        """
+        if answer.lower() == "y":
+            os.system("shutdown /s /t 1")
         else:
-            print("apps already closed")
-            close = input("Do you wish to shutdown your computer ? (y / n): ")
-            if close.lower() == "y":
-                os.system("shutdown /s /t 1")
+            exit()
+
 
 if __name__ == "__main__":
-    apps = Creating_apps_list()
-    apps_list = apps.getting_apps()
-    apps_list = apps.listing_apps(apps_list)
-    Closing_apps.close_apps(apps_list)
+    # Instance Classes
+    listing = Creating_apps_list()
+    shutting = ShuttingDown()
+    view = View()
+
+    # Executions control
+    view.client_view()
+    bar = view.progress_bar()
+    with bar:
+        apps_list = listing.getting_apps()
+        apps_list = listing.listing_apps(apps_list)
+        for i in range(50):
+            bar.update()
+            sleep(0.01)
+        ready = shutting.close_apps(apps_list)
+        for i in range(50):
+            bar.update()
+            sleep(0.01)
+        bar.close()
+        answer = view.asking_for_shutting_pc(ready)
+        shutting.shut_pc(answer)
